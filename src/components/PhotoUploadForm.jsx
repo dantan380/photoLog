f8646fce444c096397812f275f92c1d1075ce633
Daniel Tan <danieltan380@gmail.com>
2024-04-16 21:57:08 -0700

    Create conditional logic for checking valid file types.

--- src/components/PhotoUploadForm.jsx
+++ src/components/PhotoUploadForm.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 
 const PhotoUploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
 
-  const changeHandler = () => {
-    console.log('changed');
+  const allowedTypes = ['image/png', 'image/jpeg'];
+
+  const changeHandler = (e) => {
+    let selectedFile = e.target.files[0];
+    
+    if (selectedFile && allowedTypes.includes(selectedFile.type)) {
+      setFile(selectedFile)
+      setError("");
+    } else {
+      setFile(null);
+      setError("Please select an PNG or JPEG file type")
+    }
   }
 
   return (
     <form>
       <input type="file" onChange={changeHandler}/>
+      <div className="output">
+        { error && <div className="error">{ error }</div>}
+        { file && <div>{ file.name }</div>}
+      </div>
     </form>
   )
 };
