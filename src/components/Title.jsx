b6744687ebde206a20ba83a7e9b5207959c1742a
Daniel Tan <danieltan380@gmail.com>
2024-04-17 21:32:46 -0700

    Add logic to get today's date in Title component.

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,20 @@
 const Title = () => {
+
+  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+  let today = new Date();
+  let day = today.getDate();
+  let monthIndex = today.getMonth();
+  let year = today.getFullYear();
+
+  let monthName = monthNames[monthIndex];
+
+  let formattedDate = monthName + ' ' + day + ' ' + year;
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>PhotoLog</h1>
+      <h2>Welcome</h2>
+      <p>{formattedDate} </p>
     </div>
   )
 }
