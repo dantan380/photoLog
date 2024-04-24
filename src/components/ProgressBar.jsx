6495d1df3b06034428da3770d4140a05fbd64f82
Daniel Tan <danieltan380@gmail.com>
2024-04-23 19:44:50 -0700

    Create logic to remove progress bar on completion.

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,12 +1,23 @@
+import { useEffect } from "react";
 import useStorage from "../hooks/useStorage";
 
 const ProgressBar = ({file, setFile}) => {
-  const {url, progress} = useStorage(file);
-  console.log(progress, url)
+  const { url, progress } = useStorage(file);
+  
+  //If URL exists, set 'file' to null to remove progress bar upon completion.
+  useEffect(() => {
+    if (url) {
+      console.log("url exists")
+      setFile(null);
+    }
+  }, [url, setFile])
 
   return (
-    <div className="progress-bar">progress</div>
+    <div className="progress-bar" style={{ width: progress + '%' }}></div>
   )
 }
+
+//To fix ESLint error with prop types.
+ProgressBar.propTypes = null;
 
 export default ProgressBar
