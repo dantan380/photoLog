b13f6fba9458c9ff006dfaea6e02a5ffb79dbe22
Daniel Tan <danieltan380@gmail.com>
2024-04-23 20:44:14 -0700

    Create reference to collection and add new document to collection.

--- src/hooks/useStorage.jsx
+++ src/hooks/useStorage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import { projStorage } from "../firebase/config";
+import { projStorage, projFirestore } from "../firebase/config";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { serverTimestamp, collection, addDoc } from "firebase/firestore";
 
 const useStorage = (file) => {
   const [progress, setProgress] = useState(0);
@@ -16,6 +17,8 @@ const useStorage = (file) => {
     const storageRef = ref(projStorage, file.name);
     const uploadTask = uploadBytesResumable(storageRef, file, metadata);
 
+    const imagesCollectionRef = collection(projFirestore, 'images');
+
     //After each state change for the upload, take a snap shot, which is the current percentage.
     uploadTask.on('state_changed', (snap) => {
       let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
@@ -25,10 +28,16 @@ const useStorage = (file) => {
     }, () => {
        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
         console.log('File available at', downloadURL);
-        setUrl(downloadURL)
+        const createdAt = serverTimestamp();
+        const docRef = addDoc(imagesCollectionRef, {
+          createdAt: createdAt,
+          url: downloadURL
+        })
+        console.log('Document has been created at: ', docRef.createdAt);
+        setUrl(downloadURL);
       });
     })
-  }, [file, url])
+  }, [file])
 
   return { progress, url, error };
 
