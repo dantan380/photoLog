520bc785d950b4b8c0066251933f2ef1312b14f5
Daniel Tan <danieltan380@gmail.com>
2024-04-24 23:48:25 -0700

    Show images within Image grid.

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -2,11 +2,14 @@ import useFirestore from "../hooks/useFirestore";
 
 const ImageGrid = () => {
   const { docs } = useFirestore('images');
-  console.log(docs);
 
   return (
     <div className="img-grid">
-      images
+      { docs && docs.map(doc => (
+          <div className="img-wrap" key={doc.id}>
+            <img src={doc.url} alt="uploaded img" />
+          </div>
+      )) }
     </div>
   )
 };
