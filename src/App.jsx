14eac7812cbb99bf5c5178db3225faac4a8b5f10
Daniel Tan <danieltan380@gmail.com>
2024-04-24 22:43:01 -0700

    Add ImageGrid component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Title from './components/Title'
 import PhotoUploadForm from './components/PhotoUploadForm';
+import ImageGrid from './components/ImageGrid';
 
 function App() {
   return (
     <div className='App'>
       <Title/>
       <PhotoUploadForm/>
+      <ImageGrid/>
     </div>
   );
 }
