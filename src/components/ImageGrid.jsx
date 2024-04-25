1f0efb9fda383af82c638d41ff6369d2b8f1091e
Daniel Tan <danieltan380@gmail.com>
2024-04-24 22:43:27 -0700

    Create base ImageGrid component.

---
+++ src/components/ImageGrid.jsx
@@ -0,0 +1,14 @@
+import useFirestore from "../hooks/useFirestore";
+
+const ImageGrid = () => {
+  const { docs } = useFirestore('images');
+  console.log(docs);
+
+  return (
+    <div className="img-grid">
+      images
+    </div>
+  )
+};
+
+export default ImageGrid;
