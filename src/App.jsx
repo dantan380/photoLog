15705e82252f7a3d62296ea21370b669968d62af
Daniel Tan <danieltan380@gmail.com>
2024-04-27 21:56:38 -0700

    Create state for selected image.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import './App.css'
 import Title from './components/Title'
 import PhotoUploadForm from './components/PhotoUploadForm';
 import ImageGrid from './components/ImageGrid';
+import FullPage from './components/FullPage';
+import { useState } from 'react';
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
+
   return (
     <div className='App'>
       <Title/>
       <PhotoUploadForm/>
-      <ImageGrid/>
+      <ImageGrid setSelectedImg={setSelectedImg}/>
+      { selectedImg && <FullPage selectedImg={selectedImg} setSelectedImg={setSelectedImg} /> }
     </div>
   );
 }
