1dfd393a441bac1d486a3dff2384d18dd25e3946
Daniel Tan <danieltan380@gmail.com>
2024-04-30 13:30:59 -0700

    Implement deleteImage function.

--- src/components/FullPage.jsx
+++ src/components/FullPage.jsx
@@ -1,15 +1,43 @@
+import useFirestore from "../hooks/useFirestore";
 import { motion } from "framer-motion";
-import { projStorage } from "../firebase/config";
-import { deleteObject } from "firebase/storage";
+import { projStorage, projFirestore } from "../firebase/config";
+import { deleteObject, ref } from "firebase/storage";
+import { doc, deleteDoc } from "firebase/firestore";
 
 const FullPage = ({ selectedImg, setSelectedImg }) => {
+
+  //Create reference to document storage.
+  const { docs } = useFirestore('images');
 
   const handleClick = () => {
     setSelectedImg(null);
   }
 
   const deleteImage = () => {
-    console.log(selectedImg);
+    for (let image of docs) {
+      if (image.url === selectedImg) {
+        //Create image reference.
+        const imageRef = ref(projStorage, image.name);
+
+        //Call deleteObject to delete the image.
+        deleteObject(imageRef).then(() => {
+          //Create document reference.
+          const docRef = doc(projFirestore, 'images', image.id);
+
+          //Call deleteDoc to delete the document.
+          deleteDoc(docRef).then(() => {
+            console.log('document deletion successful.');
+          })
+
+          //Once both document and image are deleted, return to image grid.
+          console.log('image deletion successful.');
+          setSelectedImg(null);
+
+        }).catch((error) => {
+          console.log(error);
+        })
+      }
+    }
   }
   
   return (
